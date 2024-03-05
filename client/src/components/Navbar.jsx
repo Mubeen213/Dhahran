e70f8c28a96eee724d3f111ecd215ce90aa51d59
Mubeen213 <mdmubeen1307@gmail.com>
2024-03-05 14:04:29 +0530

    Cart state management done.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,8 +2,12 @@ import {BsCart3, BsMoonFill, BsSunFill} from 'react-icons/bs';
 import {FaBarsStaggered} from 'react-icons/fa6';
 import {NavLink} from 'react-router-dom';
 import {NavLinks} from "./NavLinks.jsx";
+import {useSelector} from "react-redux";
 
 export const Navbar = () => {
+    const numItemsInCart =  useSelector((state) => state.cartState.numItemsInCart);
+    console.log(numItemsInCart)
+
     return (
         <nav className='bg-base-200'>
             <div className='navbar align-element '>
@@ -18,7 +22,7 @@ export const Navbar = () => {
                     {/* DROPDOWN */}
                     <div className='dropdown'>
                         <label tabIndex={0} className='btn btn-ghost lg:hidden'>
-                            <FaBarsStaggered className='h-6 w-6' />
+                            <FaBarsStaggered className='h-6 w-6'/>
                         </label>
                         <ul
                             tabIndex={0}
@@ -36,9 +40,9 @@ export const Navbar = () => {
                     {/* CART LINK*/}
                     <NavLink to='cart' className='btn btn-ghost btn-circle btn-md ml-4'>
                         <div className='indicator'>
-                            <BsCart3 className='h-6 w-6' />
+                            <BsCart3 className='h-6 w-6'/>
                             <span className='badge badge-sm badge-primary indicator-item'>
-                8
+                {numItemsInCart}
               </span>
                         </div>
                     </NavLink>
--- client/src/features/cart/cartSlice.js
+++ client/src/features/cart/cartSlice.js
@@ -11,26 +11,48 @@ const defaultState = {
     orderTotal: 0
 }
 
+const getCartFromLocalStorage = () => {
+    return JSON.parse(localStorage.getItem('cart')) || defaultState;
+};
+
 const cartSlice = createSlice({
 
     name: 'cart',
-    initialState: defaultState,
+    initialState: getCartFromLocalStorage,
     reducers: {
         addItem: (state, action) => {
             const {service} = action.payload
 
-            const isItemPresent = state.cartItems.find((item) => item.cartID === service.cartID)
+            const item = state.cartItems.find((item) => item.cartID === service.cartID)
 
+            if (item) {
+                item.amount += service.amount;
+            } else {
+                state.cartItems.push(service)
+            }
+            state.numItemsInCart += service.amount;
+            state.cartTotal += service.price * service.amount;
+            cartSlice.caseReducers.calculateTotals(state)
+            toast.success('Item added to cart')
         },
+
         removeItem: (state, action) => {
             const {cartID} = action.payload;
             const service = state.cartItems.find((item) => item.cartID === cartID)
             state.cartItems = state.cartItems.filter((item) => item.cartID !== cartID)
-
-            state.numItemsInCart -= 1;
+            state.numItemsInCart -= service.amount;
+            state.cartTotal -= service.price * service.amount;
+            cartSlice.caseReducers.calculateTotals(state)
+            toast.error('Item removed from cart')
         },
         editItem: (state, action) => {
-
+            const { cartID, amount } = action.payload;
+            const item = state.cartItems.find((i) => i.cartID === cartID);
+            state.numItemsInCart += amount - item.amount;
+            state.cartTotal += item.price * (amount - item.amount);
+            item.amount = amount;
+            cartSlice.caseReducers.calculateTotals(state);
+            toast.success('Cart updated');
         },
         clearCart: (state, action) => {
             localStorage.setItem('cart', JSON.stringify(defaultState))
@@ -38,7 +60,7 @@ const cartSlice = createSlice({
 
         calculateTotals: (state) => {
             state.tax = 0.1 * state.cartTotal;
-            state.orderTotal = state.orderTotal + state.shipping + state.tax;
+            state.orderTotal = state.cartTotal + state.shipping + state.tax;
             localStorage.setItem('cart', JSON.stringify(state))
         }
     }
